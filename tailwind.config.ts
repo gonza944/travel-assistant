91cac5bc2db7195fa77c8e1480626ee0383fa6dc
Gonzalo Ariza Sabbatini <Gonza94@gmail.com>
2024-07-11 15:36:55 -0300

    Implement fonts and color pallette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { functionalTokens } from "./const";
 
 const config: Config = {
   content: [
@@ -7,14 +8,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      title: ["var(--font-freudian)", "serif"],
+    },
     extend: {
+      colors: functionalTokens,
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
 export default config;
